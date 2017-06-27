951f51667658b5696ef786ff1e7f800e0b601e7c
Josh Pollock <jmp@D-173-250-200-65.dhcp4.washington.edu>
2017-06-27 13:43:17 -0700

    2_StackMachine.lean v1 done

--- 2_StackMachine.lean
+++ 2_StackMachine.lean
@@ -1,5 +1,9 @@
-section -- Artihmetic Expressions Over Natural Numbers
-section -- Source Language (Arithmetic Expressions)
+import tools.auto.finish
+import _target.deps.mini_crush.src.mini_crush
+
+namespace arith
+-- *Artihmetic Expressions Over Natural Numbers
+-- **Source Language
 
 -- binary operation syntax
 inductive binop : Type
@@ -31,8 +35,7 @@ def expDenote : exp → ℕ
 #reduce (decidable.to_bool(expDenote (exp.Binop binop.Times (exp.Binop binop.Plus (exp.Const 2) (exp.Const 2)) (exp.Const 7)) = 28))
 #eval   (decidable.to_bool(expDenote (exp.Binop binop.Times (exp.Binop binop.Plus (exp.Const 2) (exp.Const 2)) (exp.Const 7)) = 28))
 
-end -- Source Language (Arithmetic Expressions)
-section -- Target Language (Stack Machine)
+-- **Target Language
 
 -- instruction syntax
 inductive instr : Type
@@ -64,8 +67,7 @@ def progDenote : prog → stack → option stack
     | some s' := progDenote p' s'
     end
 
-end -- Target Language (Stack Machine)
-section -- Translation
+-- **Translation
 
 def compile : exp → prog
 | (exp.Const n)       := instr.iConst n :: []
@@ -81,49 +83,187 @@ def compile : exp → prog
 #reduce progDenote (compile (exp.Binop binop.Plus (exp.Const 2) (exp.Const 2))) []
 #reduce progDenote (compile (exp.Binop binop.Times (exp.Binop binop.Plus (exp.Const 2) (exp.Const 2))(exp.Const 7))) []
 
-end -- Translation
-section -- Translation Correctness
---TODO: prove!!
-lemma compile_correct' : ∀ e p s, progDenote (compile e ++ p) s = progDenote p (expDenote e :: s) := sorry
+-- **Translation Correctness
 
-theorem compile_correct : ∀ e, progDenote (compile e) [] = some (expDenote e :: []) := sorry
+lemma compile_correct' (e : exp) : ∀ p s, progDenote (compile e ++ p) s = progDenote p (expDenote e :: s) :=
+begin
+induction e; intros,
+case exp.Const {
+    unfold compile,
+    unfold expDenote,
+    simp,
+    unfold progDenote,
+    unfold instrDenote,
+    simp,
+    unfold instrDenote._match_1,
+    unfold progDenote._match_1,
+    refl
+},
+case exp.Binop {
+    unfold compile,
+    unfold expDenote,
+    simp,    /- can replace these lines with simph -/
+    rw ih_2, /- -/
+    rw ih_1, /- -/
+    unfold progDenote,
+    unfold instrDenote,
+    simp,
+    unfold instrDenote._match_1,
+    unfold instrDenote._match_2,
+    unfold progDenote._match_1,
+    refl
+}
+end
 
+lemma compile_correct'_lean (e : exp) : ∀ p s, progDenote (compile e ++ p) s = progDenote p (expDenote e :: s) :=
+begin
+induction e; intros,
+case exp.Const {refl},
+case exp.Binop {
+    unfold compile expDenote,
+    simph,
+    refl
+}
+end
 
-end -- Translation Correctness
-end -- Artihmetic Expressions Over Natural Numbers
+@[simp] lemma compile_correct'_crush (e : exp) : ∀ p s, progDenote (compile e ++ p) s = progDenote p (expDenote e :: s) :=
+by mini_crush
 
-section  -- Typed Expressions
+lemma app_nil_end {A : Type} (l : list A) : l = l ++ list.nil := by induction l; simph
+
+theorem compile_correct (e : exp) : progDenote (compile e) [] = some (expDenote e :: []) :=
+begin
+rw app_nil_end (compile e),
+rw compile_correct',
+refl
+end
+
+-- TODO: excessive memory consumption
+theorem compile_correct_crush (e : exp) : progDenote (compile e) [] = some (expDenote e :: []) :=
+by mini_crush
+
+end arith
+
+namespace typed
+-- *Typed Expressions
+-- **Source Language
 
 -- atomic types
 inductive type : Type
 | Nat
 | Bool
+open type
 
 -- binop types
 inductive tbinop : type → type → type → Type
-| TPlus  : tbinop type.Nat type.Nat type.Nat
-| TTimes : tbinop type.Nat type.Nat type.Nat
-| TEq    : ∀ t, tbinop t t type.Bool
-| TLt    : tbinop type.Nat type.Nat type.Bool
+| TPlus  : tbinop Nat Nat Nat
+| TTimes : tbinop Nat Nat Nat
+| TEq    : ∀ t, tbinop t t Bool
+| TLt    : tbinop Nat Nat Bool
+open tbinop
 
 -- expression types
 inductive texp : type → Type
-| TNConst : ℕ → texp type.Nat
-| TBConst : bool → texp type.Bool
-| TBinop : ∀ t1 t2 t, tbinop t1 t2 t → texp t1 → texp t2 → texp t
+| TNConst : ℕ → texp Nat
+| TBConst : bool → texp Bool
+| TBinop : ∀ {t1 t2 t}, tbinop t1 t2 t → texp t1 → texp t2 → texp t
+open texp
 
--- map from source object types to meta types
+-- type denotational semantics
 def typeDenote : type → Type
-| type.Nat := ℕ
-| type.Bool := bool
+| Nat := ℕ
+| Bool := bool
 
-def tbinopDenote {arg1 arg2 res : type} (b : tbinop arg1 arg2 res) : typeDenote arg1 → typeDenote arg2 → typeDenote res :=
-match b with
-| tbinop.TPlus         := nat.add
-| tbinop.TTimes        := nat.mul
-| tbinop.TEq type.Nat  := nat.decidable_eq
-| tbinop.TEq type.Bool := bool.decidable_eq
-| tbinop.TLt           := nat.le
-end
+-- tbinop denotational semantics
+def tbinopDenote : ∀ {arg1 arg2 res} (b : tbinop arg1 arg2 res),
+    typeDenote arg1 → typeDenote arg2 → typeDenote res
+| ._ ._ ._ TPlus      := nat.add
+| ._ ._ ._ TTimes     := nat.mul
+| ._ ._ ._ (TEq Nat)  := (λ x y, decidable.to_bool $ x = y)
+| ._ ._ ._ (TEq Bool) := (λ x y, decidable.to_bool $ x = y)
+| ._ ._ ._ TLt        := (λ x y, decidable.to_bool $ nat.le x y)
 
-end -- Typed Expressions
+-- texp denotational semantics
+def texpDenote : ∀ {t}, texp t → typeDenote t
+| ._ (TNConst n) := n
+| ._ (TBConst b) := b
+| ._ (@TBinop _ _ _ b e1 e2) := (tbinopDenote b) (texpDenote e1) (texpDenote e2)
+
+#reduce texpDenote (TNConst 42)
+#reduce texpDenote (TBConst true)
+-- TODO: is there a way to remove need for _'s?
+#reduce texpDenote (TBinop TTimes (TBinop TPlus (TNConst 2) (TNConst 2)) (TNConst 7))
+#reduce texpDenote (TBinop (TEq Nat) (TBinop TPlus (TNConst 2) (TNConst 2)) (TNConst 7))
+#reduce texpDenote (TBinop TLt (TBinop TPlus (TNConst 2) (TNConst 2)) (TNConst 7))
+
+-- **Target Language
+
+-- stack type to describe how expressions affect the stack
+@[reducible] def tstack := list type
+
+inductive tinstr : tstack → tstack → Type
+| TiNConst : ∀ s, ℕ → tinstr s (Nat :: s)
+| TiBConst : ∀ s, bool → tinstr s (Bool :: s)
+| TiBinop  : ∀ arg1 arg2 res s,
+    tbinop arg1 arg2 res
+    → tinstr (arg1 :: arg2 :: s) (res :: s)
+open tinstr
+
+inductive tprog : tstack → tstack → Type
+| TNil  : ∀ s, tprog s s
+| TCons : ∀ s1 s2 s3,
+    tinstr s1 s2
+    → tprog s2 s3
+    → tprog s1 s3
+open tprog
+
+def vstack : tstack → Type
+| []      := unit
+| (t :: ts) := typeDenote t × vstack ts
+
+def tinstrDenote : ∀ {ts} {ts'}, tinstr ts ts' → vstack ts → vstack ts'
+| ._ ._ (TiNConst _ n)      := (λ s, (n, s))
+| ._ ._ (TiBConst _ b)      := (λ s, (b, s))
+| ._ ._ (TiBinop _ _ _ _ b) := (λ s,
+    let (arg1, (arg2, s')) := s in
+        ((tbinopDenote b) arg1 arg2, s'))
+
+def tprogDenote : ∀ {ts} {ts'}, tprog ts ts' → vstack ts → vstack ts'
+| ._ ._ (TNil _) := (λ s, s)
+| ._ ._ (TCons _ _ _ i p) := (λ s, tprogDenote p (tinstrDenote i s))
+
+-- **Translation
+
+def tconcat : ∀ {ts} {ts'} {ts''}, tprog ts ts' → tprog ts' ts'' → tprog ts ts''
+| ._ ._ ts'' (TNil _) := (λ p, p)
+| ._ ._ ts'' (TCons _ _ _ i p1) := (λ p, TCons _ _ _ i (tconcat p1 p))
+
+def tcompile : ∀ {t} (e : texp t) (ts : tstack), tprog ts (t :: ts)
+| ._ (TNConst n) _ := TCons _ _ _ (TiNConst _ n) (TNil _)
+| ._ (TBConst b) _ := TCons _ _ _ (TiBConst _ b) (TNil _)
+| ._ (@TBinop _ _ _ b e1 e2) _ := tconcat (tcompile e2 _)
+    (tconcat (tcompile e1 _) (TCons _ _ _ (TiBinop _ _ _ _ b) (TNil _)))
+
+#reduce tprogDenote (tcompile (TNConst 42) []) ()
+#reduce tprogDenote (tcompile (TBConst true) []) ()
+#reduce tprogDenote (tcompile (TBinop TTimes (TBinop TPlus (TNConst 2)
+    (TNConst 2)) (TNConst 7)) []) ()
+#reduce tprogDenote (tcompile (TBinop (TEq Nat) (TBinop TPlus (TNConst 2)
+    (TNConst 2)) (TNConst 7)) []) ()
+#reduce  tprogDenote (tcompile (TBinop TLt (TBinop TPlus (TNConst 2) (TNConst 2))
+    (TNConst 7)) []) ()
+
+-- **Translation Correctness
+
+@[simp] lemma tconcat_correct : ∀ ts ts' ts'' (p : tprog ts ts') (p' : tprog ts' ts'') (s : vstack ts),
+    tprogDenote (tconcat p p') s = tprogDenote p' (tprogDenote p s) :=
+by mini_crush
+
+@[simp] lemma tcompile_correct' : ∀ t (e : texp t) ts (s : vstack ts),
+    tprogDenote (tcompile e ts) s = (texpDenote e, s) :=
+by mini_crush
+
+theorem tcompile_correct : ∀ t (e : texp t),
+    tprogDenote (tcompile e []) () = (texpDenote e, ()) := by mini_crush
+
+end typed
